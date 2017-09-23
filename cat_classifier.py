efd8be61f458a02cd732a7cd5ec34e05097d4fac
Binesh Bannerjee <binesh_binesh@hotmail.com>
2017-09-23 02:11:22 -0400

    Refactored..

--- cat_classifier.py
+++ cat_classifier.py
@@ -4,7 +4,6 @@
 
 import sys
 import os
-import json
 from PIL import Image
 import numpy as np
 import tensorflow as tf
@@ -17,11 +16,16 @@ def center_crop(img):
 
 	return img.crop((rxx, ryy, rxx+rsz, ryy+rsz)).resize((256, 256), resample=Image.BICUBIC)
 
+def traverse(dirname, func):
+	if os.path.isdir(dirname):
+		children = [os.path.join(dirname, child) for child in sorted(os.listdir(dirname))]
+		for child in children:
+			traverse(child, func)
+	elif os.path.isfile(dirname):
+		func(dirname)
 
-#pylint: disable=not-context-manager,unused-variable
+#pylint: disable=not-context-manager,too-many-locals
 def main(argv):
-	tensorboarddir = "/dogs/tensorboard"
-	batchsz = 64
 	assert os.path.exists("/dogs/restore/bestsofar.ckpt.meta")
 	sys.stderr.write("Restoring %s\n" % ("/dogs/restore/bestsofar.ckpt"))
 	restorer = tf.train.import_meta_graph("/dogs/restore/bestsofar.ckpt.meta")
@@ -32,25 +36,46 @@ def main(argv):
 	# we'll need input_images_, dog_predicates_, training_ and final
 	graph = tf.get_default_graph()
 	input_images_ = graph.get_tensor_by_name('input/images:0')
-	dog_predicates_ = graph.get_tensor_by_name('targets/target:0')
 	training_ = graph.get_tensor_by_name('configuration/training_predicate:0')
-	logits_op = tf.nn.sigmoid(graph.get_tensor_by_name("final/conv2d/BiasAdd:0")) # should have named this. *sigh*
+	sigmoid_op = tf.nn.sigmoid(graph.get_tensor_by_name("final/conv2d/BiasAdd:0")) # should have named this. *sigh*
 
+	batchsz = 64
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		restorer.restore(sess, "/dogs/restore/bestsofar.ckpt")
 
-		input_images_np = np.zeros((len(argv), 256, 256, 3))
-		for idx, imgfn in enumerate(argv):
-			img = Image.open(imgfn)
-			input_images_np[idx] = 2.0 * np.array(center_crop(img)) / 255.0 - 1
-			img.close()
+		position = [0]
+		files = []
+		input_images_np = np.zeros((batchsz, 256, 256, 3))
 
-		logits_np = sess.run(logits_op, feed_dict={input_images_: input_images_np, \
-			training_: False})
-		assert len(logits_np) == len(argv)
-		json.dump(dict(zip(argv, logits_np.reshape((len(argv))).tolist())), sys.stdout, indent=4, sort_keys=True)
+		with open("/tmp/dogs.csv", "w") as dogfp:
+			def handle(files):
+				sigmoids_np = sess.run(sigmoid_op, feed_dict={input_images_: input_images_np, \
+					training_: False})
+				sigmoids_np = sigmoids_np[0:position[0]].reshape((position[0])).tolist()
+				print len(sigmoids_np), len(files)
+				assert len(sigmoids_np) == len(files)
 
+				for filename, probability in zip(files, sigmoids_np):
+					dogfp.write("%s,%.7f\n" % (filename, probability))
+					sys.stderr.write("%s,%.7f\n" % (filename, probability))
+				while files:
+					files.pop()
+
+			def process(imgfn):
+				img = Image.open(imgfn)
+				files.append(imgfn)
+				idx = position[0]
+				input_images_np[idx] = 2.0 * np.array(center_crop(img)) / 255.0 - 1
+				position[0] += 1
+				if position[0] == batchsz:
+					handle(files)
+					position[0] = 0
+
+			for dirname in argv:
+				traverse(dirname, process)
+				if position[0] > 0:
+					handle(files)
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
--- cat_trainer.py
+++ cat_trainer.py
@@ -60,13 +60,56 @@ def random_rotation(filtered):
 	# So, now what is the portion of the rotated image that
 	# is still valid?
 	denominator = abs(math.sin(theta)) + abs(math.cos(theta))
-	rotatedsz = rsz / denominator
+	rotatedsz = int(rsz / denominator)
 	return rotated.crop(( \
 		(rsz - rotatedsz) / 2, \
 		(rsz - rotatedsz) / 2, \
 		(rsz + rotatedsz) / 2, \
 		(rsz + rotatedsz) / 2 \
 	))
+
+#pylint: disable=too-many-locals,too-many-arguments
+def single_epoch(sess, epoch, info, input_images_, dog_predicates_, training_, action_op, loss_op, training):
+	evaluation_mode = "train" if training else "validation"
+	batchsz = 64
+
+	random.shuffle(info[evaluation_mode])
+	loss = 0
+	valcopy = None
+	if evaluation_mode == "validation":
+		valcopy = copy.deepcopy(info[evaluation_mode])
+	for start in xrange(0, len(info[evaluation_mode]), batchsz):
+		end = min(start + batchsz, len(info[evaluation_mode]))
+		data = info[evaluation_mode][start:end]
+		filenames, dog_predicates = zip(*data)
+		actualsz = len(data)
+		np_input_images = np.zeros((actualsz, 256, 256, 3))
+		np_dog_predicates = np.array(dog_predicates).reshape(len(data), 1)
+		for idx in xrange(0, actualsz):
+			img = Image.open(filenames[idx])
+			if training:
+				cropped = random_crop(img, 192, 384)
+				filtered = random_filter(cropped)
+				rotated = random_rotation(filtered)
+				resized = rotated.resize((256, 256), resample=Image.BICUBIC)
+			else:
+				resized = img.resize((256, 256), resample=Image.BICUBIC)
+			np_input_images[idx] = (np.array(resized) / 255.0) * 2.0 - 1.0
+
+		batchloss, preds = sess.run([loss_op, action_op], feed_dict={input_images_: np_input_images, dog_predicates_: np_dog_predicates, training_: training})
+
+		if valcopy is not None:
+			for idx in xrange(0, actualsz):
+				filename, isdog = valcopy[idx+start]
+				valcopy[idx+start] = (filename, isdog, float(preds[(idx, 0)]))
+		loss += batchloss * actualsz
+
+	loss = loss / len(info[evaluation_mode])
+	if valcopy is not None:
+		with open("/dogs/logs/%08d.json" % (epoch), "w") as logfp:
+			json.dump(valcopy, logfp, indent=4, sort_keys=True)
+
+	return loss
 
 #pylint: disable=not-context-manager,too-many-locals,too-many-statements
 def main():
@@ -86,42 +129,50 @@ def main():
 
 	with tf.variable_scope("conv1"):
 		conv1 = tf.layers.conv2d(input_images_, filters=64, kernel_size=(11, 11), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool1 = tf.layers.max_pooling2d(conv1, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped1 = tf.layers.dropout(conv1, rate=0.5, noise_shape=(1, 1, 64))
+		maxpool1 = tf.layers.max_pooling2d(dropped1, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 128x128x64
 
 	with tf.variable_scope("conv2"):
 		conv2 = tf.layers.conv2d(maxpool1, filters=128, kernel_size=(5, 5), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool2 = tf.layers.max_pooling2d(conv2, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped2 = tf.layers.dropout(conv2, rate=0.5, noise_shape=(1, 1, 128))
+		maxpool2 = tf.layers.max_pooling2d(dropped2, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 64x64x128
 
 	with tf.variable_scope("conv3"):
 		conv3 = tf.layers.conv2d(maxpool2, filters=256, kernel_size=(5, 5), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool3 = tf.layers.max_pooling2d(conv3, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped3 = tf.layers.dropout(conv3, rate=0.5, noise_shape=(1, 1, 256))
+		maxpool3 = tf.layers.max_pooling2d(dropped3, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 32x32x256
 
 	with tf.variable_scope("conv4"):
 		conv4 = tf.layers.conv2d(maxpool3, filters=256, kernel_size=(5, 5), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool4 = tf.layers.max_pooling2d(conv4, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped4 = tf.layers.dropout(conv4, rate=0.5, noise_shape=(1, 1, 256))
+		maxpool4 = tf.layers.max_pooling2d(dropped4, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 16x16x256
 
 	with tf.variable_scope("conv5"):
 		conv5 = tf.layers.conv2d(maxpool4, filters=256, kernel_size=(5, 5), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool5 = tf.layers.max_pooling2d(conv5, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped5 = tf.layers.dropout(conv5, rate=0.5, noise_shape=(1, 1, 256))
+		maxpool5 = tf.layers.max_pooling2d(dropped5, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 8x8x256
 
 	with tf.variable_scope("conv6"):
 		conv6 = tf.layers.conv2d(maxpool5, filters=256, kernel_size=(5, 5), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool6 = tf.layers.max_pooling2d(conv6, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped6 = tf.layers.dropout(conv6, rate=0.5, noise_shape=(1, 1, 256))
+		maxpool6 = tf.layers.max_pooling2d(dropped6, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 4x4x256
 
 	with tf.variable_scope("conv7"):
 		conv7 = tf.layers.conv2d(maxpool6, filters=256, kernel_size=(3, 3), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool7 = tf.layers.max_pooling2d(conv7, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped7 = tf.layers.dropout(conv7, rate=0.5, noise_shape=(1, 1, 256))
+		maxpool7 = tf.layers.max_pooling2d(dropped7, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 2x2x256
 
 	with tf.variable_scope("conv8"):
 		conv8 = tf.layers.conv2d(maxpool7, filters=1024, kernel_size=(1, 1), strides=(1, 1), padding="same", activation=activation, kernel_initializer=tf.glorot_normal_initializer())
-		maxpool8 = tf.layers.max_pooling2d(conv8, pool_size=(2, 2), strides=(2, 2), padding="same")
+		dropped8 = tf.layers.dropout(conv8, rate=0.5, noise_shape=(1, 1, 1024))
+		maxpool8 = tf.layers.max_pooling2d(dropped8, pool_size=(2, 2), strides=(2, 2), padding="same")
 		# Now, we should be at size 1x1x1024
 
 	# Normally, we'd have a fully connected (dense) layer here.
@@ -158,7 +209,6 @@ def main():
 		train_cost_scalar = tf.summary.scalar("train_cost", train_cost_)
 
 	tensorboarddir = "/dogs/tensorboard"
-	batchsz = 64
 	with tf.Session() as sess:
 		sess.run(tf.global_variables_initializer())
 		if os.path.exists(tensorboarddir):
@@ -173,59 +223,15 @@ def main():
 		with open("/dogs/images-cropped/info.json", "r") as infofp:
 			info = json.load(infofp)
 
-		best_ce = None
 		epoch = 0
+		best_ce = single_epoch(sess, epoch, info, input_images_, dog_predicates_, training_, loss_op=loss, action_op=predictions, training=False)
+		sys.stderr.write("Starting with best_ce=%.7f\n" % (best_ce))
 		while True:
 			epoch_start = time.time()
 			epoch += 1
 			# one epoch.
-			random.shuffle(info["train"])
-			trainloss = 0
-			for start in xrange(0, len(info["train"]), batchsz):
-				end = min(start + batchsz, len(info["train"]))
-				data = info["train"][start:end]
-				filenames, dog_predicates = zip(*data)
-				actualsz = len(data)
-				np_input_images = np.zeros((actualsz, 256, 256, 3))
-				np_dog_predicates = np.array(dog_predicates).reshape(len(data), 1)
-				for idx in xrange(0, actualsz):
-					img = Image.open(filenames[idx])
-					cropped = random_crop(img, 192, 384)
-					filtered = random_filter(cropped)
-					rotated = random_rotation(filtered)
-					resized = rotated.resize((256, 256), resample=Image.BICUBIC)
-					np_input_images[idx] = (np.array(resized) / 255.0) * 2.0 - 1.0
-					img.close()
-
-				batchloss, _ = sess.run([loss, step], feed_dict={input_images_: np_input_images, dog_predicates_: np_dog_predicates, training_: True})
-				trainloss += batchloss * actualsz
-			trainloss = trainloss / len(info["train"])
-
-			# Now, let's do the validation.
-			validationloss = 0
-			valcopy = copy.deepcopy(info["validation"])
-			for start in xrange(0, len(info["validation"]), batchsz):
-				end = min(start + batchsz, len(info["validation"]))
-				data = info["validation"][start:end]
-				filenames, dog_predicates = zip(*data)
-				actualsz = len(data)
-				np_input_images = np.zeros((actualsz, 256, 256, 3))
-				np_dog_predicates = np.array(dog_predicates).reshape(len(data), 1)
-				for idx in xrange(0, actualsz):
-					img = Image.open(filenames[idx])
-					resized = img.resize((256, 256), resample=Image.BICUBIC)
-					np_input_images[idx] = (np.array(resized) / 255.0) * 2.0 - 1.0
-					img.close()
-
-				[batchloss, preds] = sess.run([loss, predictions], feed_dict={input_images_: np_input_images, dog_predicates_: np_dog_predicates, training_: False})
-				for idx in xrange(0, actualsz):
-					filename, isdog = valcopy[idx+start]
-					valcopy[idx+start] = (filename, isdog, float(preds[(idx, 0)]))
-
-				validationloss += batchloss * actualsz
-			with open("/dogs/logs/%08d.json" % (epoch), "w") as logfp:
-				json.dump(valcopy, logfp, indent=4, sort_keys=True)
-			validationloss = validationloss / len(info["validation"])
+			trainloss = single_epoch(sess, epoch, info, input_images_, dog_predicates_, training_, loss_op=loss, action_op=step, training=True)
+			validationloss = single_epoch(sess, epoch, info, input_images_, dog_predicates_, training_, loss_op=loss, action_op=predictions, training=False)
 			tcost, vcost = sess.run([train_cost_scalar, validation_cost_scalar], feed_dict={train_cost_: trainloss, validation_cost_: validationloss})
 			writer.add_summary(tcost, epoch)
 			writer.add_summary(vcost, epoch)
